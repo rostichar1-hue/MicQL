c71c544ea9a70dfc79b89803e4ff236b7823f523
rostichar <rostichar1@gmail.com>
2026-07-22 23:17:07 +0300

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,13 +1,24 @@
 import sys
-from parser import parse
-from executor import execute
+import os
+from .parser import parse
+from .executor import execute
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: micro <file.mc>")
-        sys.exit(1)
+        filename = "data.mc"
+    else:
+        filename = sys.argv[1]
 
-    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print(f"Файл {filename} не найден. Создаю data.mc...")
+        with open("data.mc", "w") as f:
+            f.write("CREATE users (id INT, name STRING, age INT)\n")
+            f.write("+users {1, \"Alex\", 28}\n")
+            f.write("+users {2, \"Kate\", 22}\n")
+            f.write("users:name(age>20)10\n")
+            f.write("SAVE\n")
+        print("✅ Создан data.mc с примерами.")
+
     with open(filename) as f:
         lines = [line.strip() for line in f if line.strip() and not line.startswith('//')]
 
