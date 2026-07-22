5ecdae9641985d0e092f8d48e90762d4ebbcce45
rostichar <rostichar1@gmail.com>
2026-07-22 22:28:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mc-lang',
+    name='micql',
     version='1.0.0',
-    description='MicroCode — минималистичный язык запросов',
+    description='Micql — минималистичный язык запросов',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     author='rostichar1-hue',
-    url='https://github.com/rostichar1-hue/MicroCode',
+    url='https://github.com/rostichar1-hue/Micql',
     packages=find_packages(),
     install_requires=['tabulate'],
     entry_points={
         'console_scripts': [
-            'micro=microcode.cli:main',
+            'micro=micql.cli:main',
         ],
     },
     python_requires='>=3.6',
